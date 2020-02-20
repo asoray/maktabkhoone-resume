6b39233858e712a792307019953f00f4aade2e22
Asoray <asoray@gmail.com>
2020-02-20 20:20:45 +0330

    Updates

---
+++ precache-manifest.ed263f0478eb9f63bdc075385c47ac15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d257a9528a4e1b7852e4debd0bce0a",
+    "url": "/maktabkhoone-resume/index.html"
+  },
+  {
+    "revision": "b7f551dc79613c5ef13e",
+    "url": "/maktabkhoone-resume/static/css/main.eefb9a70.chunk.css"
+  },
+  {
+    "revision": "ed5ff18a2dd3ac95afcc",
+    "url": "/maktabkhoone-resume/static/js/2.fb9ac6fc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/maktabkhoone-resume/static/js/2.fb9ac6fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f551dc79613c5ef13e",
+    "url": "/maktabkhoone-resume/static/js/main.ac3d14c7.chunk.js"
+  },
+  {
+    "revision": "d03aafdb2cff06a04b0b",
+    "url": "/maktabkhoone-resume/static/js/runtime-main.44529cb9.js"
+  }
+]);
